8029a08694e1546f9384e4801cfae5ebbe271f6c
singularitti <singularitti@outlook.com>
2020-12-11 23:01:52 -0500

    Move `customize` to src/EosFitting/customize.jl

--- src/EosFitting/EosFitting.jl
+++ src/EosFitting/EosFitting.jl
@@ -33,37 +33,9 @@ shortname(::Type{SelfConsistentField}) = "scf"
 shortname(::Type{StOptim}) = "relax"
 shortname(::Type{VcOptim}) = "vc-relax"
 
-
-struct OutdirSetter{T} <: Setter
-    timefmt::T
-end
-OutdirSetter() = OutdirSetter(" Y-m-d H:M:S ")
-function (x::OutdirSetter)(template::PWInput)
-    @set! template.control.outdir = abspath(joinpath(
-        template.control.outdir,
-        template.control.prefix * format(now(), x.timefmt),
-    ))
-    return template
-end
-
-customize(a, b) = customize(b, a)  # If no method found, switch arguments & try again
-function customize(pressure::Pressure, volume::Volume)
-    function _customize(template::PWInput)::PWInput
-        set = OutdirSetter() ∘ VolumeSetter(volume) ∘ PressureSetter(pressure)
-        return set(template)
-    end
-end
-function customize(pressure::Pressure, eos::PressureEOS)
-    function _customize(template::PWInput)::PWInput
-        volume = mustfindvolume(eos, pressure; volume_scale = (0.5, 1.5))
-        set = OutdirSetter() ∘ VolumeSetter(volume) ∘ PressureSetter(pressure)
-        return set(template)
-    end
-end
-customize(pressure::Pressure, eos::EquationOfStateOfSolids) =
-    customize(pressure, PressureEOS(getparam(eos)))
 include("settings.jl")
 include("standardize.jl")
+include("customize.jl")
 
 function parseoutput(::SelfConsistentField)
     function (file)
