f359314f4cafb51d6d1e15f124f1fcc215b03b85
singularitti <singularitti@outlook.com>
2020-12-11 22:56:28 -0500

    Simplify `customize` using closures

--- src/EosFitting.jl
+++ src/EosFitting.jl
@@ -136,21 +136,22 @@ function (x::OutdirSetter)(template::PWInput)
     return template
 end
 
-function customize(template::PWInput, pressure::Pressure, volume::Volume)::PWInput
-    set = OutdirSetter() ∘ VolumeSetter(volume) ∘ PressureSetter(pressure)
-    return set(template)
+customize(a, b) = customize(b, a)  # If no method found, switch arguments & try again
+function customize(pressure::Pressure, volume::Volume)
+    function _customize(template::PWInput)::PWInput
+        set = OutdirSetter() ∘ VolumeSetter(volume) ∘ PressureSetter(pressure)
+        return set(template)
+    end
 end
-customize(template::PWInput, volume::Volume, pressure::Pressure) =
-    customize(template, pressure, volume)
-function customize(template::PWInput, pressure::Pressure, eos::PressureEOS)::PWInput
-    volume = mustfindvolume(eos, pressure; volume_scale = (0.5, 1.5))
-    set = OutdirSetter() ∘ VolumeSetter(volume) ∘ PressureSetter(pressure)
-    return set(template)
+function customize(pressure::Pressure, eos::PressureEOS)
+    function _customize(template::PWInput)::PWInput
+        volume = mustfindvolume(eos, pressure; volume_scale = (0.5, 1.5))
+        set = OutdirSetter() ∘ VolumeSetter(volume) ∘ PressureSetter(pressure)
+        return set(template)
+    end
 end
-customize(template::PWInput, pressure::Pressure, eos::EquationOfStateOfSolids) =
-    customize(template, pressure, PressureEOS(getparam(eos)))
-customize(template::PWInput, eos::EquationOfStateOfSolids, pressure::Pressure) =
-    customize(template, pressure, eos)
+customize(pressure::Pressure, eos::EquationOfStateOfSolids) =
+    customize(pressure, PressureEOS(getparam(eos)))
 
 function parseoutput(::SelfConsistentField)
     function (file)
