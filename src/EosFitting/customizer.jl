d5610eb7b5795f9cfd816e84f651ac33f63c273f
singularitti <singularitti@outlook.com>
2020-12-12 20:23:26 -0500

    Deprecate `vscaling`, use `vscale` in `Customizer`

--- src/EosFitting/customizer.jl
+++ src/EosFitting/customizer.jl
@@ -13,15 +13,20 @@ end
 struct Customizer{A,B}
     a::A
     b::B
+    vscale::NTuple{2,Float64}
 end
+Customizer(a, b) = Customizer(a, b, (0.5, 1.5))
 function (x::Customizer{<:Pressure,<:Volume})(template::PWInput)::PWInput
     customize = OutdirSetter() ∘ VolumeSetter(x.b) ∘ PressureSetter(x.a)
     return customize(template)
 end
 function (x::Customizer{<:Pressure,<:PressureEOS})(template::PWInput)
-    volume = mustfindvolume(x.b, x.a; volume_scale = (0.5, 1.5))
-    return Customizer(x.a, volume)(template)
+    volume = mustfindvolume(x.b, x.a; volume_scale = x.vscale)
+    x = @set! x.b = volume
+    return x(template)
 end
-(x::Customizer{<:Pressure,<:EquationOfStateOfSolids})(template::PWInput) =
-    Customizer(x.a, PressureEOS(getparam(x.b)))(template)
-(x::Customizer)(template::PWInput) = Customizer(x.b, x.a)(template)  # If no method found, switch arguments & try again
+function (x::Customizer{<:Pressure,<:EquationOfStateOfSolids})(template::PWInput)
+    x = @set! x.b = PressureEOS(getparam(x.b))
+    return x(template)
+end
+(x::Customizer)(template::PWInput) = Customizer(x.b, x.a, x.vscale)(template)  # If no method found, switch arguments & try again
