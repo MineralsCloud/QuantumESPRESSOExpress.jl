2866c64f87290fa38941321c96324c77308dc753
singularitti <singularitti@outlook.com>
2020-12-15 02:11:33 -0500

    Delete eos.jl & phonon.jl because we now have XPS.jl

--- examples/GaN/eos.jl
+++
@@ -1,6 +0,0 @@
-using Express: buildworkflow
-using QuantumESPRESSOExpress.EosFitting
-using SimpleWorkflow: run!
-
-x = buildworkflow("examples/GaN/eos.yaml")
-run!(x)
--- examples/GaN/phonon.jl
+++
@@ -1,6 +0,0 @@
-using Express: buildworkflow
-using QuantumESPRESSOExpress.Phonon
-using SimpleWorkflow: run!
-
-x = buildworkflow("examples/GaN/phonon.yaml")
-run!(x)
