f30aa702a03713c36b7fb3d305883e32f1412b65
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-17 01:52:02 -0400

    Fix `_materialize_tmpl` to `materialize` for `AbstractString`

--- src/EosFitting/Config.jl
+++ src/EosFitting/Config.jl
@@ -1,21 +1,12 @@
 module Config
 
-using Crystallography: cellvolume
-using QuantumESPRESSO.Inputs.PWscf:
-    CellParametersCard, PWInput, VerbositySetter, VolumeSetter, PressureSetter
+using QuantumESPRESSO.Inputs.PWscf: PWInput
 
 import Express.EquationOfStateWorkflow.Config: materialize
 
-function _materialize_tmpl(config, fixed)
-    arr = map(config.paths) do file
-        str = read(expanduser(file), String)
-        parse(PWInput, str)
-    end
-    if length(arr) != 1  # Length of `templates` = length of `pressures`
-        return arr
-    else
-        return repeat(arr, length(fixed.values))
-    end
+function materialize(template::AbstractString)
+    str = read(expanduser(template), String)
+    return parse(PWInput, str)
 end
 
 end
