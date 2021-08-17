f902d1cd87ecf1d47219ac177c4883eebbaf625e
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-17 01:50:40 -0400

    Remove `materialize` for `AbstractDict`

--- src/EosFitting/Config.jl
+++ src/EosFitting/Config.jl
@@ -1,13 +1,10 @@
 module Config
 
-using Configurations: from_dict, @option
 using Crystallography: cellvolume
-using Express.EosFitting.Config: Pressures, Volumes, EosFittingConfig
-using QuantumESPRESSO.Cli: QuantumESPRESSOCliConfig
 using QuantumESPRESSO.Inputs.PWscf:
     CellParametersCard, PWInput, VerbositySetter, VolumeSetter, PressureSetter
 
-import Express.EosFitting.Config: materialize
+import Express.EquationOfStateWorkflow.Config: materialize
 
 function _materialize_tmpl(config, fixed)
     arr = map(config.paths) do file
@@ -19,30 +16,6 @@ function _materialize_tmpl(config, fixed)
     else
         return repeat(arr, length(fixed.values))
     end
-end
-
-function materialize(config::AbstractDict)
-    config = from_dict(EosFittingConfig{QuantumESPRESSOCliConfig}, config)
-
-    templates = _materialize_tmpl(config.templates, config.fixed)
-
-    fixed = if config.fixed === nothing
-        # If no volume or pressure is provided, use templates cell volumes
-        Volumes(map(cellvolume, config.templates))
-    else
-        config.fixed
-    end |> materialize
-
-    return (
-        templates = templates,
-        fixed = fixed,
-        trial_eos = materialize(config.trial_eos),
-        workdir = config.dirs.root,
-        dirs = materialize(config.dirs, config.fixed),
-        inv_opt = config.inv_opt,
-        recover = config.recover,
-        cli = config.cli,
-    )
 end
 
 end
