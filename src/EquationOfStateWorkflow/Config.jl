d1578c46764c1c25aea27f0cd83da0486f8bfe8c
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-20 05:13:18 -0400

    Fix `materialize` for `AbstractDict` by overloading `Configurations.convert_to_option`

--- src/EquationOfStateWorkflow/Config.jl
+++ src/EquationOfStateWorkflow/Config.jl
@@ -1,12 +1,25 @@
 module Config
 
 using QuantumESPRESSO.Inputs.PWscf: PWInput
+using QuantumESPRESSO.Commands:
+    QuantumESPRESSOConfig, PwxConfig, PhxConfig, Q2rxConfig, MatdynxConfig
+using AbInitioSoftwareBase.Commands: CommandConfig, MpiexecConfig
 
-import Express.EquationOfStateWorkflow.Config: materialize
+import Configurations: convert_to_option
+import Express.EquationOfStateWorkflow.Config: RuntimeConfig, materialize
 
 function materialize(template::AbstractString)
     str = read(expanduser(template), String)
     return parse(PWInput, str)
 end
+
+convert_to_option(::Type{RuntimeConfig}, ::Type{CommandConfig}, dict) =
+    QuantumESPRESSOConfig(;
+        mpi = get(dict, "mpi", MpiexecConfig()),
+        pw = get(dict, "pw", PwxConfig()),
+        ph = get(dict, "ph", PhxConfig()),
+        q2r = get(dict, "q2r", Q2rxConfig()),
+        matdyn = get(dict, "matdyn", MatdynxConfig()),
+    )
 
 end
