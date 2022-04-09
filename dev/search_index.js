aed5cf73759f1b16a7e3e4c3e229e8e8065b7eef
Documenter.jl <documenter@juliadocs.github.io>
2022-04-09 21:25:51 +0000

    build based on 36d34a9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuantumESPRESSOExpress","category":"page"},{"location":"#QuantumESPRESSOExpress","page":"Home","title":"QuantumESPRESSOExpress","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuantumESPRESSOExpress.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuantumESPRESSOExpress]","category":"page"}]
+}
