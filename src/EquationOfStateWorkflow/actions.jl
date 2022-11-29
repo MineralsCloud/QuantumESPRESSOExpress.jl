8a95408a0118d0a94d3311eca2ee9b29415358cf
singularitti <singularitti@outlook.com>
2022-11-29 06:54:12 -0500

    Fix importing of `Calculation` types

--- src/EquationOfStateWorkflow/actions.jl
+++ src/EquationOfStateWorkflow/actions.jl
@@ -2,7 +2,7 @@ using AbInitioSoftwareBase: parentdir
 using AbInitioSoftwareBase.Inputs: Setter
 using Dates: format, now
 using EquationsOfStateOfSolids: PressureEquation, Parameters, getparam, vsolve
-using ExpressBase: Scf, FixedCellOptimization, VariableCellOptimization
+using Express.EquationOfStateWorkflow: Scf, FixedCellOptimization, VariableCellOptimization
 using QuantumESPRESSO.Commands: pw
 using QuantumESPRESSO.Inputs.PWscf: PWInput, VerbositySetter, VolumeSetter, PressureSetter
 using Setfield: @set!
@@ -24,8 +24,10 @@ function (x::CalculationSetter)(template::PWInput)
         "scf"
     elseif x.calc isa FixedCellOptimization
         "relax"
-    else
+    elseif x.calc isa VariableCellOptimization
         "vc-relax"
+    else
+        throw(ArgumentError("this should never happen!"))
     end
     return template
 end
