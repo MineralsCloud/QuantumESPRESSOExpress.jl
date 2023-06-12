203a3dece80799aae0c673bea9a34ee4cc769009
singularitti <singularitti@outlook.com>
2023-06-12 14:09:15 -0400

    Change extending `parseoutput` to `ExtractData`

--- src/EquationOfStateWorkflow/EquationOfStateWorkflow.jl
+++ src/EquationOfStateWorkflow/EquationOfStateWorkflow.jl
@@ -11,40 +11,36 @@ using QuantumESPRESSO.PWscf:
 using Unitful: @u_str
 using UnitfulAtomic
 
-import Express.EquationOfStateWorkflow: parseoutput
+import Express.EquationOfStateWorkflow: ExtractData
 
 include("Config.jl")
 include("actions.jl")
 # include("DB.jl")
 
-function parseoutput(::Scf)
-    function (file)
-        str = read(file, String)
-        preamble = tryparse(Preamble, str)
-        e = try
-            parse_electrons_energies(str, :converged)
-        catch
-        end
-        if preamble !== nothing && !isempty(e)
-            return preamble.omega * u"bohr^3" => e.ε[end] * u"Ry"  # volume, energy
-        else
-            return nothing
-        end
+function (::ExtractData{Scf})(file)
+    str = read(file, String)
+    preamble = tryparse(Preamble, str)
+    e = try
+        parse_electrons_energies(str, :converged)
+    catch
+    end
+    if preamble !== nothing && !isempty(e)
+        return preamble.omega * u"bohr^3" => e.ε[end] * u"Ry"  # volume, energy
+    else
+        return nothing
     end
 end
-function parseoutput(::VariableCellOptimization)
-    function (file)
-        str = read(file, String)
-        if !isjobdone(str)
-            @warn "Job is not finished!"
-        end
-        x = tryparsefinal(CellParametersCard, str)
-        if x !== nothing
-            return cellvolume(parsefinal(CellParametersCard, str)) * u"bohr^3" =>
-                parse_electrons_energies(str, :converged).ε[end] * u"Ry"  # volume, energy
-        else
-            return nothing
-        end
+function (::ExtractData{VariableCellOptimization})(file)
+    str = read(file, String)
+    if !isjobdone(str)
+        @warn "Job is not finished!"
+    end
+    x = tryparsefinal(CellParametersCard, str)
+    if x !== nothing
+        return cellvolume(parsefinal(CellParametersCard, str)) * u"bohr^3" =>
+            parse_electrons_energies(str, :converged).ε[end] * u"Ry"  # volume, energy
+    else
+        return nothing
     end
 end
 
