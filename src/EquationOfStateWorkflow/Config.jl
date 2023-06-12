d6fee1b94d79067bb6484ee655ad651989b78a45
singularitti <singularitti@outlook.com>
2023-06-12 13:53:42 -0400

    Move `ParallelizationFlags`, `QuantumESPRESSOConfig`, `PwxConfig` from `QuantumESPRESSOCommands` to here

--- src/EquationOfStateWorkflow/Config.jl
+++ src/EquationOfStateWorkflow/Config.jl
@@ -1,8 +1,7 @@
 module Config
 
-using Configurations: OptionField
+using Configurations: OptionField, @option
 using QuantumESPRESSO.PWscf: PWInput
-using QuantumESPRESSO.Commands: QuantumESPRESSOConfig, PwxConfig
 using AbInitioSoftwareBase.Commands: CommandConfig, MpiexecConfig
 
 import Configurations: from_dict
@@ -11,6 +10,120 @@ import Express.EquationOfStateWorkflow.Config: RuntimeConfig, ExpandConfig
 function (::ExpandConfig)(template::AbstractString)
     str = read(expanduser(template), String)
     return parse(PWInput, str)
+end
+
+"""
+    ParallelizationFlags(; nimage=0, npool=0, ntg=0, nyfft=0, nband=0, ndiag=0)
+
+Construct parallelization flags of QuantumESPRESSO commands.
+"""
+@option mutable struct ParallelizationFlags
+    nimage::UInt = 0
+    npool::UInt = 0
+    ntg::UInt = 0
+    nyfft::UInt = 0
+    nband::UInt = 0
+    ndiag::UInt = 0
+end
+
+"""
+    PwxConfig(; path, chdir, options)
+
+Create configurations for `pw.x`.
+
+# Arguments
+- `path::String="pw.x"`: the path to the executable.
+- `chdir::Bool=true`: whether to change directory to where the input file is
+  stored when running `pw.x`. If `false`, stay in the current directory.
+- `options::ParallelizationFlags=ParallelizationFlags()`: the parallelization
+  flags of `pw.x`.
+"""
+@option mutable struct PwxConfig <: CommandConfig
+    path::String = "pw.x"
+    chdir::Bool = true
+    options::ParallelizationFlags = ParallelizationFlags()
+    env::Union{Dict,Vector} = Dict(ENV)
+end
+"""
+    PhxConfig(; path, chdir, options)
+
+Create configurations for `ph.x`.
+
+# Arguments
+- `path::String="ph.x"`: the path to the executable.
+- `chdir::Bool=true`: whether to change directory to where the input file is
+  stored when running `ph.x`. If `false`, stay in the current directory.
+- `options::ParallelizationFlags=ParallelizationFlags()`: the parallelization
+  flags of `ph.x`.
+"""
+@option mutable struct PhxConfig <: CommandConfig
+    path::String = "ph.x"
+    chdir::Bool = true
+    options::ParallelizationFlags = ParallelizationFlags()
+    env::Union{Dict,Vector} = Dict(ENV)
+end
+"""
+    Q2rxConfig(; path, chdir, options)
+
+Create configurations for `q2r.x`.
+
+# Arguments
+- `path::String="q2r.x"`: the path to the executable.
+- `chdir::Bool=true`: whether to change directory to where the input file is
+  stored when running `q2r.x`. If `false`, stay in the current directory.
+- `options::ParallelizationFlags=ParallelizationFlags()`: the parallelization
+  flags of `q2r.x`.
+"""
+@option mutable struct Q2rxConfig <: CommandConfig
+    path::String = "q2r.x"
+    chdir::Bool = true
+    options::ParallelizationFlags = ParallelizationFlags()
+    env::Union{Dict,Vector} = Dict(ENV)
+end
+"""
+    MatdynxConfig(; path, chdir, options)
+
+Create configurations for `matdyn.x`.
+
+# Arguments
+- `path::String="matdyn.x"`: the path to the executable.
+- `chdir::Bool=true`: whether to change directory to where the input file is
+  stored when running `matdyn.x`. If `false`, stay in the current directory.
+- `options::ParallelizationFlags=ParallelizationFlags()`: the parallelization
+  flags of `matdyn.x`.
+"""
+@option mutable struct MatdynxConfig <: CommandConfig
+    path::String = "matdyn.x"
+    chdir::Bool = true
+    options::ParallelizationFlags = ParallelizationFlags()
+    env::Union{Dict,Vector} = Dict(ENV)
+end
+"""
+    DynmatxConfig(; path, chdir, options)
+
+Create configurations for `dynmat.x`.
+
+# Arguments
+- `path::String="dynmat.x"`: the path to the executable.
+- `chdir::Bool=true`: whether to change directory to where the input file is
+  stored when running `dynmat.x`. If `false`, stay in the current directory.
+- `options::ParallelizationFlags=ParallelizationFlags()`: the parallelization
+  flags of `dynmat.x`.
+"""
+@option mutable struct DynmatxConfig <: CommandConfig
+    path::String = "dynmat.x"
+    chdir::Bool = true
+    options::ParallelizationFlags = ParallelizationFlags()
+    env::Union{Dict,Vector} = Dict(ENV)
+end
+
+@option mutable struct QuantumESPRESSOConfig <: CommandConfig
+    mpi::MpiexecConfig = MpiexecConfig()
+    pw::PwxConfig = PwxConfig()
+    ph::PhxConfig = PhxConfig()
+    q2r::Q2rxConfig = Q2rxConfig()
+    matdyn::MatdynxConfig = MatdynxConfig()
+    dynmat::DynmatxConfig = DynmatxConfig()
 end
 
 function from_dict(::Type{RuntimeConfig}, ::OptionField{:cli}, ::Type{CommandConfig}, dict)
