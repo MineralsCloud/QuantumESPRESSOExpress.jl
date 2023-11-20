473418c78e01443a00cc855e6ce2159af33baca3
singularitti <singularitti@outlook.com>
2023-11-20 01:19:19 -0500

    Add src/MD/Config.jl

---
+++ src/MD/Config.jl
@@ -0,0 +1,26 @@
+module Config
+
+using Configurations: OptionField
+using ExpressBase.Config: SoftwareConfig
+using QuantumESPRESSO.PWscf: PWInput
+
+using ...QuantumESPRESSOExpress: QuantumESPRESSOConfig, MpiexecConfig, PwxConfig
+
+import Configurations: from_dict
+import Express.EquationOfState.Config: StaticConfig, _update!
+
+function _update!(conf, template::AbstractString)
+    str = read(expanduser(template), String)
+    conf.template = parse(PWInput, str)
+    return conf
+end
+
+function from_dict(
+    ::Type{<:StaticConfig}, ::OptionField{:cli}, ::Type{SoftwareConfig}, dict
+)
+    return QuantumESPRESSOConfig(;
+        mpi=get(dict, "mpi", MpiexecConfig()), pw=get(dict, "pw", PwxConfig())
+    )
+end
+
+end
